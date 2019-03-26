ce6e35fc6c70d8dec878031fc26afbacde055ca7
efdssdf <efdssdf@gmail.com>
2019-03-26 17:41:32 +0800

    'fix'

--- util/image.js
+++ util/image.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const request = require('request');
 const exec = require('child_process').exec;
+const gm = require('gm').subClass({imageMagick: true});
 process.env.PATH += ":/usr/local/GraphicsMagick-1.3.28/bin";
 
 function downloadHead(uri, filename, callback) {
@@ -10,8 +11,8 @@ function downloadHead(uri, filename, callback) {
 
 function result_img(head, name, backimgurl, callback) {
     if (name && head) {
-        var mosaic_cmd = 'gm "convert" "-page" "+0+0" "' + __dirname + '/user_image/' + backimgurl + '" "-page" "+163+1042" "' + __dirname + '/user_image/smallhead_'
-            + head + '" "-mosaic" "' + __dirname + '/user_image/' + head + '"'
+        var mosaic_cmd = 'gm "convert" "-page" "+0+0" "' + backimgurl + '" "-page" "+87+203" "' + __dirname + '/user_image/smallhead_'
+            + head + '" "-font" "' + name + '" "-mosaic" "' + __dirname + '/result_image/' + head + '"'
         console.log(mosaic_cmd, '--------------------mosaic_cmd')
 
         exec(mosaic_cmd, function (error, stdout, stderr) {
@@ -33,10 +34,21 @@ function getUserImg(name, headimgurl, backimgurl, callback) {
             if (err) {
                 console.log(err, '------------------err')
             }
-            var resize_head = 'gm "convert" "' + __dirname + '/user_image/head_' + head + '" "-resize" "167x" "' + __dirname + '/user_image/smallhead_' + head + '"';
-            exec(resize_head, function (errorhead, stdouthead, stderrhead) {
-                result_img(head, name, backimgurl)
-            });
+            let origin = __dirname + '/user_image/head_' + head;
+            let output = __dirname + '/user_image/smallhead_' + head;
+            let size = 300;
+            gm(origin)
+                .crop(233, 233, 29, 26)
+                .resize(size, size)
+                .write(output, function () {
+                    gm(size, size, 'none')
+                        .fill(output)
+                        .drawCircle(size / 2, size / 2, size / 2, 0)
+                        .write(output, function (err) {
+                            console.log(err || 'done');
+                            result_img(head, name, backimgurl)
+                        });
+                });
         })
     } else {
         result_img('', name, backimgurl)
