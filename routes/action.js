8668ea83fd8ef0dc65575b2031d9f4bdde77c6b9
Administrator <lionel@buzzy.cc>
2019-03-26 19:33:33 +0800

    1

--- routes/action.js
+++ routes/action.js
@@ -40,7 +40,7 @@ router.get('/dth_res',async(ctx,next)=>{
 
 	imageUtil.getUserImg(name,user.headimgurl,str_bg,function(head){
 		if(head){
-			ctx.redirect('/action/result?img='+encodeURIComponent(head))
+			ctx.redirect('/action/result.html?img='+encodeURIComponent(head))
 			//next()
 		}else{
 			ctx.redirect('/action/dth');
