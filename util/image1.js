e5d79159ed11c5bcfa39954a4066ae85c24b5938
efdssdf <efdssdf@gmail.com>
2019-03-27 18:30:52 +0800

    'image1'

--- util/image1.js
+++ util/image1.js
@@ -7,11 +7,12 @@ function downloadHead(uri, filename, callback) {
     request(uri).pipe(stream).on('close', callback);
 }
 
-function result_img(backimg, header, name, pic_name, callback) {
-    if (backimg) {
-        gm().in('-page', '+0+0').in(backimg.backimgurl)
-            .in('-page', '+' + header.head_x + '+' + header.head_y + '').in(__dirname + '/user_image/smallhead_' + pic_name)
-            .font(__dirname + '/' + name.name_typeface).fontSize(name.name_size).fill(name.name_colour).drawText(name.name_x, name.name_y, name.name)
+function result_img(backimg, header, name, pic_name, code, callback) {
+    if (backimg.url) {
+        gm().in('-page', '+0+0').in(backimg.url)
+            .in('-page', '+' + header.x + '+' + header.y + '').in(__dirname + '/user_image/smallhead_' + pic_name)
+            .in('-page', '+' + code.x + '+' + code.y + '').in(code.url)
+            .font(__dirname + '/' + name.typeface).fontSize(name.size).fill(name.colour).drawText(name.x, name.y, name.name)
             .mosaic()
             .write(__dirname + '/../public/action_img/result_image/' + pic_name, function (error) {
                 if (error) {
@@ -29,8 +30,9 @@ function getUserImg(param, callback) {
     let backimg = param.backimg
     let header = param.header
     let name = param.name
-    if (header.headimgurl) {
-        downloadHead(header.headimgurl, __dirname + '/user_image/head_' + pic_name, function (err1, res) {
+    let code = param.code
+    if (header.url) {
+        downloadHead(header.url, __dirname + '/user_image/head_' + pic_name, function (err1, res) {
             if (err1) {
                 console.log(err1, '------------------err1')
             }
@@ -46,12 +48,12 @@ function getUserImg(param, callback) {
                         .drawCircle(size / 2, size / 2, size / 2, 0)
                         .write(output, function (err) {
                             console.log(err || 'done');
-                            result_img(backimg, header, name, pic_name, callback)
+                            result_img(backimg, header, name, pic_name, code, callback)
                         });
                 });
         })
     } else {
-        result_img('', '', '', '', callback)
+        result_img('', '', '', '', '', callback)
     }
 }
 
